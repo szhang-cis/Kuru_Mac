a514928978da2356ef03dda2ee7e445974bfd07e
Joan-Dario LAUBRIE-SOTO <joan.laubrie@emse.fr>
2020-01-20 18:10:29 +0100

    cythonize2

---
+++ Kuru/FiniteElements/Assembly/_Assembly_/FollowerForces.h
@@ -0,0 +1,387 @@
+#ifndef FOLLOWER_FORCES_H
+#define FOLLOWER_FORCES_H
+
+#include <algorithm>
+
+#ifdef __SSE4_2__
+#include <emmintrin.h>
+#include <mm_malloc.h>
+#endif
+
+#include <Fastor.h>
+#include "SparseAssemblyNative.h"
+
+#ifndef LL_TYPES
+#define LL_TYPES
+using Real = double;
+using Integer = std::int64_t;
+using UInteger = std::uint64_t;
+#endif
+
+/*---------------------------------------------------------------------------------------------*/
+#ifndef CUSTOM_ALLOCATION_
+#define CUSTOM_ALLOCATION_
+template<typename T>
+FASTOR_INLINE T *allocate(Integer size) {
+#if defined(__AVX__)
+    T *out = (T*)_mm_malloc(sizeof(T)*size,32);
+#elif defined(__SSE__)
+    T *out = (T*)_mm_malloc(sizeof(T)*size,16);
+#else
+    T *out = (T*)malloc(sizeof(T)*size);
+#endif
+    return out;
+}
+
+template<typename T>
+FASTOR_INLINE void deallocate(T *a) {
+#if defined(__SSE__)
+    _mm_free(a);
+#else
+    free(a);
+#endif
+}
+#endif //CUSTOM_ALLOCATION_
+/*---------------------------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------------------------*/
+#ifndef SPARSE_TRIPLET_FILLER
+#define SPARSE_TRIPLET_FILLER
+// IJV Filler
+FASTOR_INLINE
+void fill_triplet(  const Integer *i,
+                    const Integer *j,
+                    const Real *coeff,
+                    int *I,
+                    int *J,
+                    Real *V,
+                    Integer elem,
+                    Integer nvar,
+                    Integer nodeperelem,
+                    const UInteger *elements,
+                    Integer i_shape,
+                    Integer j_shape
+                    ) {
+
+    Integer ndof = nvar*nodeperelem;
+    Integer *current_row_column = allocate<Integer>(nvar*nodeperelem);
+
+    Integer const_elem_retriever;
+    for (Integer counter=0; counter<nodeperelem; ++counter) {
+        const_elem_retriever = nvar*elements[elem*nodeperelem+counter];
+        for (Integer ncounter=0; ncounter<nvar; ++ncounter) {
+            current_row_column[nvar*counter+ncounter] = const_elem_retriever+ncounter;
+        }
+    }
+
+    Integer icounter = 0;
+    Integer ncounter = ndof*ndof*elem;
+
+    Integer const_I_retriever;
+    for (Integer counter=0; counter<ndof; ++counter) {
+        const_I_retriever = current_row_column[counter];
+        for (Integer iterator=0; iterator<ndof; ++iterator) {
+            I[ncounter] = const_I_retriever;
+            J[ncounter] = current_row_column[iterator];
+            V[ncounter] = coeff[icounter];
+            ncounter++;
+            icounter++;
+        }
+    }
+
+    deallocate(current_row_column);
+}
+
+
+
+FASTOR_INLINE
+void fill_global_data(
+                    const Integer *i,
+                    const Integer *j,
+                    const Real *coeff,
+                    int *I,
+                    int *J,
+                    Real *V,
+                    Integer elem,
+                    Integer nvar,
+                    Integer nodeperelem,
+                    const UInteger *elements,
+                    Integer i_shape,
+                    Integer j_shape,
+                    int recompute_sparsity_pattern,
+                    int squeeze_sparsity_pattern,
+                    const int *data_local_indices,
+                    const int *data_global_indices,
+                    const UInteger *sorted_elements,
+                    const Integer *sorter
+    )
+{
+
+    if (recompute_sparsity_pattern) {
+        fill_triplet(i,j,coeff,I,J,V,elem,nvar,nodeperelem,elements,i_shape,j_shape);
+    }
+    else {
+        if (squeeze_sparsity_pattern) {
+            SparseAssemblyNativeCSR_RecomputeDataIndex_(
+                coeff,
+                J,
+                I,
+                V,
+                elem,
+                nvar,
+                nodeperelem,
+                sorted_elements,
+                sorter);
+        }
+        else {
+            const int ndof = nvar*nodeperelem;
+            const int local_capacity = ndof*ndof;
+            SparseAssemblyNativeCSR_(
+                coeff,
+                data_local_indices,
+                data_global_indices,
+                elem,
+                local_capacity,
+                V
+                );
+        }
+    }
+}
+
+#endif //SPARSE_TRIPLET_FILLER
+/*---------------------------------------------------------------------------------------------*/
+
+
+/*---------------------------------------------------------------------------------------------*/
+void GetFacesForces(Real *stiff_face,
+	            Real *force,
+		    const Real pressure,
+		    const Real *Bases,
+		    const Real *Jm,
+		    const Real *AllGauss,
+		    const Real *EulerElemCoords,
+		    Integer nodeperface,
+		    Integer ngauss,
+		    Integer nvar)
+{
+   Integer ndof = nodeperface*nvar;
+   Integer local_capacity = ndof*ndof;
+   Integer i;
+   Integer j;
+   Integer k;
+   Integer l;
+   Integer m;
+   Integer n;
+   
+   Real *alternating = (Real*)malloc(3*3*3*sizeof(Real)); //alternating[i*3*3 + j*3 + k]
+   std::fill(alternating,alternating+3*3*3,0.0);          //              j k     k
+   alternating[0*3*3 + 1*3 + 2] =  1.0;
+   alternating[0*3*3 + 2*3 + 1] = -1.0;
+   alternating[1*3*3 + 0*3 + 2] = -1.0;
+   alternating[1*3*3 + 2*3 + 0] =  1.0;
+   alternating[2*3*3 + 0*3 + 1] =  1.0;
+   alternating[2*3*3 + 1*3 + 0] = -1.0;
+
+   Real *N           = (Real*)malloc(ndof*nvar*ngauss*sizeof(Real));
+   Real *gNx         = (Real*)malloc(ndof*nvar*ngauss*sizeof(Real));
+   Real *gNy         = (Real*)malloc(ndof*nvar*ngauss*sizeof(Real));
+   Real *tangentialx = (Real*)malloc(ngauss*nvar*sizeof(Real));
+   Real *tangentialy = (Real*)malloc(ngauss*nvar*sizeof(Real));
+   Real *normal      = (Real*)malloc(ngauss*nvar*sizeof(Real));
+   Real *crossx      = (Real*)malloc(ndof*ndof*nvar*ngauss*sizeof(Real));
+   Real *crossy      = (Real*)malloc(ndof*ndof*nvar*ngauss*sizeof(Real));
+
+   std::fill(N,N+ndof*nvar*ngauss,0.0);
+   std::fill(gNx,gNx+ndof*nvar*ngauss,0.0);
+   std::fill(gNy,gNy+ndof*nvar*ngauss,0.0);
+   // Loop to fill function spaces for dimensons
+   // 0::3=>0,3,6,9 -- 1::3=>1,4,7,10 -- 2::3=>2,5,8,11
+   for (i=0; i<nvar; ++i) {
+      for (j=0; j<nodeperface; ++j) {
+         Integer idof = j*nvar + i;
+         for (k=0; k<ngauss; ++k) {
+            N[idof*nvar*ngauss+i*ngauss+k] = Bases[j*ngauss+k];
+            gNx[idof*nvar*ngauss+i*ngauss+k] = Jm[0*nodeperface*ngauss+j*ngauss+k];
+            gNy[idof*nvar*ngauss+i*ngauss+k] = Jm[1*nodeperface*ngauss+j*ngauss+k];
+	 }
+      }
+   }
+   std::fill(tangentialx,tangentialx+ngauss*nvar,0.0);
+   std::fill(tangentialy,tangentialy+ngauss*nvar,0.0);
+   // mapping tangential vectors [\partial\vec{x}/ \partial\zeta (ngauss x ndim)]
+   //tangentialx = einsum("ij,ik->jk",gBasesx,EulerElemCoords)
+   //tangentialy = einsum("ij,ik->jk",gBasesy,EulerElemCoords)
+   for (j=0; j<ngauss; ++j) {
+      for (k=0; k<nvar; ++k) {
+         for (i=0; i<nodeperface; ++i) {
+            tangentialx[j*nvar+k] += Jm[0*nodeperface*ngauss+i*ngauss+j]*EulerElemCoords[i*nvar+k];
+            tangentialy[j*nvar+k] += Jm[1*nodeperface*ngauss+i*ngauss+j]*EulerElemCoords[i*nvar+k];
+	 }
+      }
+   }
+   std::fill(normal,normal+ngauss*nvar,0.0);
+   // mapping normal (ngauss x ndim)
+   //normal = einsum("ijk,lj,lk->li",alternating,tangentialx,tangentialy)
+   for (l=0; l<ngauss; ++l) {
+      for (i=0; i<nvar; ++i) {
+         for (j=0; j<nvar; ++j) {
+            for (k=0; k<nvar; ++k) {
+               normal[l*nvar+i] += alternating[i*nvar*nvar+j*nvar+k]*tangentialx[l*nvar+j]*tangentialy[l*nvar+k];
+	    }
+	 }
+      }
+   }
+   // Gauss quadrature of follower load (traction)
+   //force = einsum("ijk,kj,k->ik",N,normal,AllGauss[:,0]).sum(axis=1)
+   for (i=0; i<ndof; ++i) {
+      for (k=0; k<ngauss; ++k) {
+         for (j=0; j<nvar; ++j) {
+            force[i] += pressure*N[i*nvar*ngauss+j*ngauss+k]*normal[k*nvar+j]*AllGauss[k];
+	 }
+      }
+   }
+   std::fill(crossx,crossx+ndof*ndof*nvar*ngauss,0.0);
+   std::fill(crossy,crossy+ndof*ndof*nvar*ngauss,0.0);
+   // Gauss quadrature of follower load (stiffness)
+   //crossy=einsum("ijk,ljm,nkm->lnim",alternating,gNy,N)-np.einsum("ijk,ljm,nkm->lnim",alternating,N,gNy)
+   //crossx=einsum("ijk,ljm,nkm->lnim",alternating,gNx,N)-np.einsum("ijk,ljm,nkm->lnim",alternating,N,gNx)
+   for (l=0; l<ndof; ++l) {
+      for (n=0; n<ndof; ++n) {
+         for (i=0; i<nvar; ++i) {
+            for (m=0; m<ngauss; ++m) {
+               for (j=0; j<(nvar; ++j) {
+                  for (k=0; k<nvar; ++k) {
+                     crossy[l*ndof*nvar*ngauss+n*nvar*ngauss+i*ngauss+m] += 
+		      alternating[i*nvar*nvar+j*nvar+k]*gNy[l*nvar*ngauss+j*ngauss+m]*
+		      N[n*nvar*ngauss+k*ngauss+m] - alternating[i*nvar*nvar+j*nvar+k]*
+		      N[l*nvar*ngauss+j*ngauss+m]*gNy[n*nvar*ngauss+k*ngauss+m];
+                     crossx[l*ndof*nvar*ngauss+n*nvar*ngauss+i*ngauss+m] += 
+                      alternating[i*nvar*nvar+j*nvar+k]*gNx[l*nvar*ngauss+j*ngauss+m]*
+		      N[n*nvar*ngauss+k*ngauss+m] - alternating[i*nvar*nvar+j*nvar+k]*
+		      N[l*nvar*ngauss+j*ngauss+m]*gNx[n*nvar*ngauss+k*ngauss+m];
+		  }
+	       }
+	    }
+	 }
+      }
+   }
+   //quadrature1 = einsum("ij,klji,i->kli",tangentialx,crossy,AllGauss[:,0]).sum(axis=2)
+   //quadrature2 = einsum("ij,klji,i->kli",tangentialy,crossx,AllGauss[:,0]).sum(axis=2)
+   //0.5*pressure*(quadrature1-quadrature2)
+   for (k=0; k<ndof; ++k) {
+      for (l=0; l<ndof; ++l) {
+         for (i=0; i<ngauss; ++i) {
+            for (j=0; j<nvar; ++j) {
+               stiff_face[k*ndof+l] += 0.5*pressure*(tangentialx[i*nvar+j]*
+		crossy[k*ndof*nvar*ngauss+l*nvar*ngauss+j*ngauss+i] - 
+		tangentialy[i*nvar+j]*crossx[k*ndof*nvar*ngauss+l*nvar*ngauss+j*ngauss+i])*AllGauss[i];
+            }
+	 }
+      }
+   }
+   free(alternating);
+   free(N);
+   free(gNx);
+   free(gNy);
+   free(tangentialx);
+   free(tangentialy);
+   free(normal);
+   free(crossx);
+   free(crossy);
+}
+
+void StaticForcesAssembler(const UInteger *faces,
+                           const Real *Eulerx,
+                           const Real *Bases,
+                           const Real *Jm,
+                           const Real *AllGauss,
+                           const int *pressure_flags,
+                           const Real *applied_pressure,
+                           const Real pressure_increment,
+                           int recompute_sparsity_pattern,
+                           int squeeze_sparsity_pattern,
+                           const int *data_global_indices,
+                           const int *data_local_indices,
+                           cosnt UInteger *sorted_elements,
+                           const Integer *sorter,
+                           int *I_stiff,
+                           int *J_stiff,
+                           Real *V_stiff,
+                           Real *F,
+                           Integer nface,
+                           Integer nodeperface,
+                           Integer ngauss,
+                           Integer local_size,
+                           Integer nvar)
+{
+   Integer ndof = nodeperface*nvar;
+   Integer local_capacity = ndof*ndof;
+
+   Real *EulerElemCoords = (Real*)malloc(sizeof(Real)*nodeperface*ndim);
+
+   Real *force = (Real*)malloc(ndof*sizeof(Real));
+   Real *stiff_face = (Real*)malloc(local_capacity*sizeof(Real));
+
+   // LOOP OVER FACES
+   for (Integer face=0; face<nface; ++face) {
+      if pressure_flags[face] {
+	 // APPLIED PRESSURE BY INCREMENT AND FACE
+         Real pressure = pressure_increment*applied_pressure[face];
+	 // GET FIELD AT ELEMENT LEVEL (JUST CURRENT)
+         for (Integer i=0; i<nodeperface; ++i) {
+            Integer inode = faces[face*nodeperface+i];
+            for (Integer j=0; j<nvar; ++j) {
+               EulerElemCoords[i*nvar+j] = Eulerx[inode*nvar+j];
+	    }
+	 }
+	 // COMPUTE STIFFNESS AND FORCE
+	 std::fill(force,force+ndof,0.0);
+	 std::fill(stiff_face,stiff_face+local_capacity,0.0);
+	 FacesForces(    stiff_face,
+			 force,
+			 pressure,
+			 Bases,
+			 Jm,
+			 AllGauss,
+			 EulerElemCoords,
+			 nodeperface,
+			 ngauss,
+			 nvar);
+
+         // ASSEMBLE CONSTITUTIVE STIFFNESS
+         fill_global_data(
+                nullptr,
+                nullptr,
+                stiff_face,
+                I_stiff,
+                J_stiff,
+                V_stiff,
+                face,
+                nvar,
+                nodeperface,
+                faces,
+                local_capacity,
+                local_capacity,
+                recompute_sparsity_pattern,
+                squeeze_sparsity_pattern,
+                data_local_indices,
+                data_global_indices,
+                sorted_elements,
+		sorter);
+
+	 // ASSEMBLE FORCES
+	 // F[faces[face,:]*nvar+ivar,0]+=force[ivar::nvar,0]
+	 for (i=0; i<nodeperface; ++i) {
+	    inode = faces[face*nodeperface+i]*nvar;
+	    for (ivar=0; ivar<nvar; ++ivar) {
+	       F[inode+ivar] += force[i*nvar+ivar];
+	    }
+	 }
+      }
+   }
+   free(force);
+   free(stiff_face);
+   free(EulerElemCoords);
+}
+/*---------------------------------------------------------------------------------------------*/
+#endif  //FOLLOWER_FORCES_H
--- Kuru/FiniteElements/Assembly/_Assembly_/FollowerForces.pyx
+++ Kuru/FiniteElements/Assembly/_Assembly_/FollowerForces.pyx
@@ -1,29 +1,53 @@
 import numpy as np
 cimport numpy as np
 from libc.stdint cimport int64_t, uint64_t
-from libc.stdlib cimport malloc, free
 
 ctypedef int64_t Integer
 ctypedef uint64_t UInteger
 ctypedef double Real
 
-from .SparseAssemblyNative import SparseAssemblyNative, SparseAssemblyNativeCSR_RecomputeDataIndex #, SparseAssemblyNativeCSR
-from .RHSAssemblyNative import RHSAssemblyNative
-
+cdef extern from "FollowerForces.h" nogil:
+    void AssemblerStaticForces(const UInteger *faces,
+                               const Real *Eulerx,
+                               const Real *Bases,
+                               const Real *Jm,
+                               const Real *AllGauss,
+                               const int *pressure_flags,
+                               const Real *applied_pressure,
+                               const Real pressure_increment,
+                               int recompute_sparsity_pattern,
+                               int squeeze_sparsity_pattern,
+                               const int *data_global_indices,
+                               const int *data_local_indices,
+                               cosnt UInteger *sorted_elements,
+                               const Integer *sorter,
+                               int *I_stiff,
+                               int *J_stiff,
+                               Real *V_stiff,
+                               Real *F,
+                               Integer nface,
+                               Integer nodeperface,
+                               Integer ngauss,
+                               Integer local_size,
+                               Integer nvar)
 
 def StaticForces(boundary_condition, mesh, material, function_space, fem_solver, Real[:,::1] Eulerx):
+    """
+    This routine assembles the stiffness matrix and forces vector from pressure over the material
+    so the number of variables is same than the number of dimensions of deformation.
+    nvar = ndim
+    """
 
     # GET VARIABLES FOR DISPATCHING TO C
+    cdef Integer ndim    = material.ndim
+    cdef Integer nvar    = material.ndim
+
     cdef np.ndarray[int,ndim=1, mode='c'] pressure_flags    = boundary_condition.pressure_flags
     cdef np.ndarray[Real,ndim=1, mode='c'] applied_pressure = boundary_condition.applied_pressure
     cdef Real pressure_increment                            = boundary_condition.pressure_increment
 
-    cdef Integer ndim    = material.ndim
-    cdef Integer nvar    = material.nvar
-
     cdef np.ndarray[Real,ndim=2, mode='c'] Bases         = function_space.Bases
-    cdef np.ndarray[Real,ndim=2, mode='c'] gBasesx       = function_space.gBasesx
-    cdef np.ndarray[Real,ndim=2, mode='c'] gBasesy       = function_space.gBasesy
+    cdef np.ndarray[Real,ndim=3, mode='c'] Jm            = function_space.Jm
     cdef np.ndarray[Real,ndim=1, mode='c'] AllGauss      = function_space.AllGauss.flatten()
     cdef Integer ngauss                                  = function_space.AllGauss.shape[0]
     cdef Integer local_size                              = function_space.Bases.shape[0]*nvar
@@ -73,8 +97,7 @@ def StaticForces(boundary_condition, mesh, material, function_space, fem_solver,
     AssemblerStaticForces(  &faces[0,0],
                             &Eulerx[0,0],
                             &Bases[0,0],
-                            &gBasesx[0,0],
-                            &gBasesy[0,0],
+                            &Jm[0,0,0],
                             &AllGauss[0],
                             &pressure_flags[0],
                             &applied_pressure[0],
@@ -93,147 +116,11 @@ def StaticForces(boundary_condition, mesh, material, function_space, fem_solver,
                             nodeperface,
                             ngauss,
                             local_size,
-                            nvar,
-                            ndim)
+                            nvar)
 
     if fem_solver.recompute_sparsity_pattern:
         return I_stiff, J_stiff, V_stiff, F
     else:
         return V_stiff, F
-
-cdef StaticForcesAssembler(const UInteger *faces,
-                           const Real *Eulerx,
-                           const Real *Bases,
-                           const Real *gBasesx,
-                           const Real *gBasesy,
-                           const Real *AllGauss,
-                           const int *pressure_flags,
-                           const Real *applied_pressure,
-                           const Real pressure_increment,
-                           int recompute_sparsity_pattern,
-                           int squeeze_sparsity_pattern,
-                           const int *data_global_indices,
-                           const int *data_local_indices,
-                           cosnt UInteger *sorted_elements,
-                           const Integer *sorter,
-                           int *I_stiff,
-                           int *J_stiff,
-                           Real *V_stiff,
-                           Real *F,
-                           Integer nface,
-                           Integer nodeperface,
-                           Integer ngauss,
-                           Integer local_size,
-                           Integer nvar,
-                           Integer ndim):
-
-    cdef Integer ndof = nodeperface*nvar
-    cdef Integer local_capacity = ndof*ndof
-
-    # usar calloc para iniciar en zeros
-    cdef Real *N = <Real*>malloc(sizeof(Real)*ndof*nvar*ngauss)
-    cdef Real *gNx = <Real*>malloc(sizeof(Real)*ndof*nvar*ngauss)
-    cdef Real *gNy = <Real*>malloc(sizeof(Real)*ndof*nvar*ngauss)
-    cdef Real *alternating = <Real*>malloc(sizeof(Real)*3*3*3) #alternating[i*3*3 + j*3 + k]
-    alternating[0*3*3 + 1*3 + 2]=1.                            #              j k     k
-    alternating[0*3*3 + 2*3 + 1]=-1.
-    alternating[1*3*3 + 0*3 + 2]=-1.
-    alternating[1*3*3 + 2*3 + 0]=1.
-    alternating[2*3*3 + 0*3 + 1]=1.
-    alternating[2*3*3 + 1*3 + 0]=-1.
-
-    #0::3=>0,3,6,9 #1::3=>1,4,7,10 #2::3=>2,5,8,11
-    cdef Integer ivar
-    cdef Integer inode
-    cdef Integer idof
-    cdef Integer igauss
-    for ivar in range(nvar):
-        for inode in range(nodeperface):
-            idof = inode*nvar + ivar
-            for igauss in range(ngauss):
-                N[idof*nvar*ngauss + ivar*ngauss + igauss] = Bases[inode*ngauss + igauss]
-                gNx[idof*nvar*ngauss + ivar*ngauss + igauss] = gBasesx[inode*ngauss + igauss]
-                gNy[idof*nvar*ngauss + ivar*ngauss + igauss] = gBasesy[inode*ngauss + igauss]
-
-    # Compute stiffness_matrix and force_vector
-    cdef Integer i
-    cdef Integer j
-    cdef Integer k
-    cdef Real *EulerElemCoords = <Real*>malloc(sizeof(Real)*nodeperface*ndim)
-    for face in range(nface):
-        if pressure_flags[face]:
-            pressure = pressure_increment*applied_pressure[face]
-
-            for i in range(nodeperface):
-                inode = faces[face*nodeperface + i]
-                for j in range(ndim):
-                    EulerElemCoords[i*ndim + j] = Eulerx[inode*ndim + j]
-
-            # mapping tangential vectors [\partial\vec{x}/ \partial\zeta (ngauss x ndim)]
-            tangential1 = np.einsum("ij,ik->jk",gBasesx,EulerElemCoords)
-            tangential2 = np.einsum("ij,ik->jk",gBasesy,EulerElemCoords)
-            for j in range(ngauss):
-                for k in range(ndim):
-                    for i in range(nodeperface):
-                        tangential1[j*ndim + k] += gBasesx[i*ngauss + j]*EulerElemCoords[i*ndim + k]
-                        tangential2[j*ndim + k] += gBasesy[i*ngauss + j]*EulerElemCoords[i*ndim + k]
-            # mapping normal (ngauss x ndim)
-            normal = np.einsum("ijk,lj,lk->li",alternating,tangential1,tangential2)
-            for l in range(ngauss):
-                for i in range(ndim):
-                    for j in range(ndim):
-                        for k in range(ndim):
-                            normal[l*ndim+i] += alternating[i*ndim*ndim+j*ndim+k]*tangential1[l*ndim+j]*tangential2[l*ndim+k]
-            # Gauss quadrature of follower load (traction)
-            force = np.einsum("ijk,kj,k->ik",N,normal,AllGauss[:,0]).sum(axis=1)
-            for i in range(ndof):
-                for k in range(ngauss):
-                    for j in range(nvar):
-                        force[i*ngauss+k] += N[i*nvar*ngauss+j*ngauss+k]*normal[k*nvar+j]*AllGauss[k]
-            force=pressure*force
-            # Gauss quadrature of follower load (stiffness)
-            cross1 = np.einsum("ijk,ljm,nkm->lnim",alternating,gNy,N)-np.einsum("ijk,ljm,nkm->lnim",alternating,N,gNy)
-            cross2 = np.einsum("ijk,ljm,nkm->lnim",alternating,gNx,N)-np.einsum("ijk,ljm,nkm->lnim",alternating,N,gNx)
-            for l in range(ndof):
-                for n in range(ndof):
-                    for i in range(ndim):
-                        for m in range(ngauss):
-                            for j in range(ndim):
-                                for k in range(ndim):
-                                    cross1[l*ndof*ndim*ngauss+n*ndim*ngauss+i*ngauss+m] += 
-                                        alternating[i*ndim*ndim+j*ndim+k]*gNy[l*ndim*ngauss+j*ngauss+m]*N[n*ndim*ngauss+k*ngauss+m]
-                                        -alternating[i*ndim*ndim+j*ndim+k]*N[l*ndim*ngauss+j*ngauss+m]*gNy[n*ndim*ngauss+k*ngauss+m]
-                                    cross2[l*ndof*ndim*ngauss+n*ndim*ngauss+i*ngauss+m] += 
-                                        alternating[i*ndim*ndim+j*ndim+k]*gNx[l*ndim*ngauss+j*ngauss+m]*N[n*ndim*ngauss+k*ngauss+m]
-                                        -alternating[i*ndim*ndim+j*ndim+k]*N[l*ndim*ngauss+j*ngauss+m]*gNx[n*ndim*ngauss+k*ngauss+m]
-            quadrature1 = np.einsum("ij,klji,i->kli",tangential1,cross1,AllGauss[:,0]).sum(axis=2)
-            quadrature2 = np.einsum("ij,klji,i->kli",tangential2,cross2,AllGauss[:,0]).sum(axis=2)
-            for k in range(ndof):
-                for l in range(ndof):
-                    for i in range(ngauss):
-                        for j in range(ndim):
-                            quadrature1[k*ndof*ngauss+l*ngauss+i] += tangential1[i*ndim+j]*cross1[k*ndof*ndim*ngauss+l*ndim*ngauss+j*ngauss+i]*AllGauss[i]
-                            quadrature2[k*ndof*ngauss+l*ngauss+i] += tangential2[i*ndim+j]*cross2[k*ndof*ndim*ngauss+l*ndim*ngauss+j*ngauss+i]*AllGauss[i]
-            stiffnessfa = pressure*0.5*(quadrature1-quadrature2)
-
-            I_stiff_face = np.repeat(np.arange(0,local_size),local_size,axis=0)
-            J_stiff_face = np.tile(np.arange(0,local_size),local_size)
-            V_stiff_face = stiffnessfa.ravel()
-
-            if recompute_sparsity_pattern:
-                # SPARSE ASSEMBLY - STIFFNESS MATRIX
-                SparseAssemblyNative(I_stiff_face,J_stiff_face,V_stiff_face,I_stiff,J_stiff,V_stiff,
-                    face,nvar,nodeperface,faces)
-            else:
-                if squeeze_sparsity_pattern:
-                    # SPARSE ASSEMBLY - STIFFNESS MATRIX
-                    SparseAssemblyNativeCSR_RecomputeDataIndex(mesh,V_stiff_face,I_stiff,J_stiff,V_stiff,face,ndim)
-                else:
-                    # SPARSE ASSEMBLY - STIFFNESS MATRIX
-                    V_stiff[data_global_indices[face*local_capacity:(face+1)*local_capacity]] \
-                        += V_stiff_face[data_local_indices[face*local_capacity:(face+1)*local_capacity]]
-
-            RHSAssemblyNative(F,np.ascontiguousarray(f[:,None]),face,nvar,nodeperface,faces)
-
 
 
